f7da48ee37391eb3955574619f56b4b0cee29ccf
Subhendu_Ca <ghoshshuvendu73@gmail.com>
2025-12-19 05:25:36 +0530

    done

--- src/api/auth.api.tsx
+++ src/api/auth.api.tsx
@@ -2,7 +2,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  //   baseURL: "http://localhost:5000/api",
+  baseURL: "https://re-eat-backend.onrender.com/api",
 });
 
 export const registerUser = (data: any) =>
